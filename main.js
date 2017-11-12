78f7e068e4e3cd3b9f16c1806535d7951e55c67c
austinbahng <abahng@usc.edu>
2017-11-12 14:04:09 -0800

    electron setup

---
+++ main.js
@@ -0,0 +1,19 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+// Listen and wait for app to be ready
+app.on('ready',function(){
+	// create the window
+	mainWindow = new BrowserWindow({});
+	// load html
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'WebContent/index.html'),
+		protocol: 'file:',
+		slahes: true
+	}));
+});
