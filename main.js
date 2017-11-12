3883512902105906ae7c3df3be3c55cc0c4dc08c
austinbahng <abahng@usc.edu>
2017-11-12 15:50:56 -0800

    added html for login, homepage, public itineraries

--- main.js
+++ main.js
@@ -10,9 +10,10 @@ let mainWindow;
 app.on('ready',function(){
 	// create the window
 	mainWindow = new BrowserWindow({});
+	//mainWindow.setMenu(null);
 	// load html
 	mainWindow.loadURL(url.format({
-		pathname: path.join(__dirname, 'WebContent/login.jsp'),
+		pathname: path.join(__dirname, 'WebContent/login.html'),
 		protocol: 'file:',
 		slahes: true
 	}));
